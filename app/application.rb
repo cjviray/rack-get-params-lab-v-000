59b68a028527955cc28da44bb95dd47fc6c8fa72
Christian V <viray.christianj@gmail.com>
2021-04-14 02:39:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,6 +14,16 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+    else 
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end 
+    end 
+   elsif req.path.match(/add/)
+   
     else
       resp.write "Path Not Found"
     end
