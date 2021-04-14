a69a529a87580583f4a9c6ab4d30647d8821de3a
Christian V <viray.christianj@gmail.com>
2021-04-14 02:51:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,18 +22,17 @@ class Application
         resp.write "#{item}\n"
       end 
     end 
-   elsif req.path.match(/add/)
+      elsif req.path.match(/add/)
     add_item = req.params["item"]
-    if @@items.include? add_item
+      if @@items.include? add_item
       @@cart << add_item
       resp.write "added #{add_item}"
-    else
+      else
       resp.write "We don't have that item"
-    end 
+      end 
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
